3258e4a9a3509beeff248eefa75aec2cfb416fe5
Gerardo Tordoya <zherar7ordoya@outlook.com>
2025-06-09 13:01:12 -0300

    Update Usuario_Index.js

--- SistemaVenta/SistemaVenta.AplicacionWeb/wwwroot/js/vistas/Usuario_Index.js
+++ SistemaVenta/SistemaVenta.AplicacionWeb/wwwroot/js/vistas/Usuario_Index.js
@@ -12,46 +12,73 @@ const MODELO_BASE = {
 
 let tablaData;
 
-$(document).ready(function () {
+$(function ()
+{
+    fetch('/Usuarios/ListarRoles')
+        .then(response =>
+        {
+            return response.ok ? response.json() : Promise.reject(response);
+        })
+        .then(responseJson =>
+        {
+            if (responseJson.lenght > 0)
+            {
+                responseJson.forEach(item =>
+                {
+                    $("#cboRol").append
+                        (
+                            $("<option>").val(item.idRol).text(item.descripcion)
+                        );
+                });
+            }
+        });
+
+
     tablaData = $('#tbdata').DataTable({
         responsive: true,
-        "ajax": {
+        "ajax":
+        {
             "url": '/Usuarios/ListarUsuarios',
             "type": "GET",
             "datatype": "json"
         },
-        "columns": [
-            { "data": "idUsuario", "visible": false, "searchable": false },
-            {
-                "data": "urlFoto", render: function (data) {
-                    return `<img style="height: 60px"
-                                  src=${data}
-                                  class="rounded mx-auto d-block" />`;
+        "columns":
+            [
+                { "data": "idUsuario", "visible": false, "searchable": false },
+                {
+                    "data": "urlFoto", render: function (data)
+                    {
+                        return `<img style="height: 60px" src=${data} class="rounded mx-auto d-block" />`;
+                    }
+                },
+                { "data": "nombre" },
+                { "data": "correo" },
+                { "data": "telefono" },
+                { "data": "nombreRol" },
+                {
+                    "data": "esActivo", render: function (data)
+                    {
+                        if (data == 1)
+                        {
+                            return '<span class="badge badge-info">Activo</span>';
+                        }
+                        else
+                        {
+                            return '<span class="badge badge-danger">No Activo</span>';
+                        }
+                    }
+                },
+                {
+                    "defaultContent":
+                        '<button class="btn btn-primary btn-editar btn-sm mr-2">' +
+                        '<i class="fas fa-pencil-alt"></i></button>' +
+                        '<button class="btn btn-danger btn-eliminar btn-sm">' +
+                        '<i class="fas fa-trash-alt"></i></button>',
+                    "orderable": false,
+                    "searchable": false,
+                    "width": "80px"
                 }
-            },
-            { "data": "nombre" },
-            { "data": "correo" },
-            { "data": "telefono" },
-            { "data": "nombreRol" },
-            {
-                "data": "esActivo", render: function (data) {
-                    if (data == 1)
-                        return '<span class="badge badge-info">Activo</span>';
-                    else
-                        return '<span class="badge badge-danger">No Activo</span>';
-                }
-            },
-            {
-                "defaultContent":
-                    '<button class="btn btn-primary btn-editar btn-sm mr-2">' +
-                    '<i class="fas fa-pencil-alt"></i></button>' +
-                    '<button class="btn btn-danger btn-eliminar btn-sm">' +
-                    '<i class="fas fa-trash-alt"></i></button>',
-                "orderable": false,
-                "searchable": false,
-                "width": "80px"
-            }
-        ],
+            ],
         order: [[0, "desc"]],
         dom: "Bfrtip",
         buttons: [
@@ -60,18 +87,21 @@ $(document).ready(function () {
                 extend: 'excelHtml5',
                 title: '',
                 filename: 'Reporte Usuarios',
-                exportOptions: {
+                exportOptions:
+                {
                     columns: [2, 3, 4, 5, 6]
                 }
             }, 'pageLength'
         ],
-        language: {
+        language:
+        {
             url: "https://cdn.datatables.net/plug-ins/1.11.5/i18n/es-ES.json"
         },
     });
-})
+});
 
-function mostrarModal(modelo = MODELO_BASE) {
+function mostrarModal(modelo = MODELO_BASE)
+{
     $("#txtId").val(modelo.idUsuario);
     $("#txtNombre").val(modelo.nombre);
     $("#txtCorreo").val(modelo.correo);
@@ -84,7 +114,7 @@ function mostrarModal(modelo = MODELO_BASE) {
     $("#modalData").modal("show");
 }
 
-$("#btnNuevo").click(function ()
+$("#btnNuevo").on("click", function ()
 {
     mostrarModal();
-})
+});
