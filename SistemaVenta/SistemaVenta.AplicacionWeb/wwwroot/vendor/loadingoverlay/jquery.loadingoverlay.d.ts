954b8a50612d0e4ada300a6bbbb8f7f62cd6aad4
Gerardo Tordoya <zherar7ordoya@outlook.com>
2025-06-11 15:39:13 -0300

    LoadingOverlay

---
+++ SistemaVenta/SistemaVenta.AplicacionWeb/wwwroot/vendor/loadingoverlay/jquery.loadingoverlay.d.ts
@@ -0,0 +1,9 @@
+// file: jquery.loadingoverlay.d.ts
+
+interface JQueryStatic {
+    LoadingOverlay(action: 'show' | 'hide', options?: any): void;
+}
+
+interface JQuery {
+    LoadingOverlay(action: 'show' | 'hide', options?: any): void;
+}
